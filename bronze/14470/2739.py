277e3e3b454623e8ca303d9ae8fb93f895e52eee
DoGukKim <doguk1994@gmail.com>
2025-12-11 14:54:44 +0900

    백준 2739 문제 풀이

--- bronze/14470/2739.py
+++ bronze/14470/2739.py
@@ -4,3 +4,8 @@ import sys
 
 read = sys.stdin if sys.platform == "linux" else open("input.txt")
 input = read.read().strip().split("\n")
+
+n = int(input[0])
+
+for i in range(1, 10):
+    print(f"{n} * {i} = {n * i}")
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// TimeComplexity: O(C(V, M) * N^2)
-// SpaceComplexity: O(N^2 + V)
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
